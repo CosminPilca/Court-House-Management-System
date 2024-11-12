68bd31aea2058ca95441fd8540d998fce6967699
andreipuscas <andreipuscas2004@gmail.com>
2024-11-12 15:18:37 +0200

    added by Andrei

---
+++ Repository/IRepository.java
@@ -0,0 +1,11 @@
+package com.courthouse.repository;
+
+import java.util.List;
+
+public interface IRepository<T> {
+    void create(T entity);
+    T read(String id);
+    void update(T entity);
+    void delete(String id);
+    List<T> getAll();
+}
