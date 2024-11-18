565d9b663c69b8524b72beaca5eb1efa1f48318c
andreipuscas <andreipuscas2004@gmail.com>
2024-11-18 11:38:12 +0200

    added by Andrei

--- Model/Courtroom.java
+++ Model/Courtroom.java
@@ -17,4 +17,16 @@ public class Courtroom {
         return roomNumber;
     }
 
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
 }
